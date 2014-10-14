1e597e1d90b344e42b6e729276c7d5354ef2fd1c
nikhilpi <nikhilpai2016@u.northwestern.edu>
2014-10-14 14:44:43 -0500

    new db

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/nikhilpi',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://<user>:<password>@linus.mongohq.com:10055/app30699063',
 	assets: {
 		lib: {
 			css: [
